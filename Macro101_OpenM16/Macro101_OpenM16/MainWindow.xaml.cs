5fc7bc2e2230d6a6d19fdadebc0c0f9c8716b7d4
Peter K. <119176205+PK1310@users.noreply.github.com>
2025-02-27 10:05:34 +0100

    Add files via upload

---
+++ Macro101_OpenM16/Macro101_OpenM16/MainWindow.xaml.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Macro101_OpenM16
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Laenge_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonFarbeZurück();
+            string strEingabe;
+            int intEingabe;
+            bool boolCheckEingabe;
+            strEingabe =txtBox_Laenge.Text;
+            boolCheckEingabe = CheckEingabe(strEingabe);
+
+            if (boolCheckEingabe == true)
+            {
+                intEingabe = Convert.ToInt32(strEingabe);
+                ButtonFarbe(intEingabe);
+            }
+            else return;
+            
+            
+        }
+        private bool CheckEingabe(string strEingabe)
+        {//prüfe ob eine Zahl ohne Komma eingegeben wurde
+            int intEingabe;
+            try
+            {
+                intEingabe = Convert.ToInt32(strEingabe);
+            }
+            catch
+            {
+                MessageBox.Show("Bitte eine Zahl eingeben (ohne Komma)");
+                txtBox_Laenge.Text = "";
+                txtBox_Laenge.Focus();
+                return false;
+            }
+            return true;
+        }
+        private void ButtonFarbeZurück()
+        {
+            btn_M16x25.Background = Brushes.LightGray;
+            btn_M16x30.Background = Brushes.LightGray;
+            btn_M16x35.Background = Brushes.LightGray;
+            btn_M16x40.Background = Brushes.LightGray;
+            btn_M16x45.Background = Brushes.LightGray;
+            btn_M16x50.Background = Brushes.LightGray;
+            btn_M16x55.Background = Brushes.LightGray;
+            btn_M16x60.Background = Brushes.LightGray;
+            btn_M16x65.Background = Brushes.LightGray;
+            btn_M16x70.Background = Brushes.LightGray;
+            btn_M16x80.Background = Brushes.LightGray;
+            btn_M16x90.Background = Brushes.LightGray;
+
+        }
+        private void ButtonFarbe(int Klemmung)
+        {
+            if (Klemmung < 5)
+            {
+                btn_M16x30.Background = Brushes.Yellow;
+            }
+            if (Klemmung > 4 && Klemmung < 11)
+            {
+                btn_M16x35.Background = Brushes.Yellow;
+            }
+            if (Klemmung > 10 && Klemmung < 16)
+            {
+                btn_M16x40.Background = Brushes.Yellow;
+            }
+            if (Klemmung > 15 && Klemmung < 21)
+            {
+                btn_M16x45.Background = Brushes.Yellow;
+            }
+            if (Klemmung > 20 && Klemmung < 26)
+            {
+                btn_M16x50.Background = Brushes.Yellow;
+            }
+            if (Klemmung > 25 && Klemmung < 31)
+            {
+                btn_M16x55.Background = Brushes.Yellow;
+            }
+            if (Klemmung > 30 && Klemmung < 36)
+            {
+                btn_M16x60.Background = Brushes.Yellow;
+            }
+            if (Klemmung > 35 && Klemmung < 46)
+            {
+                btn_M16x70.Background = Brushes.Yellow;
+            }
+            if (Klemmung > 45 && Klemmung < 56)
+            {
+                btn_M16x80.Background = Brushes.Yellow;
+            }
+            if (Klemmung > 55 && Klemmung < 66)
+            {
+                btn_M16x90.Background = Brushes.Yellow;
+            }
+
+        }
+        private void btn_M16x25_Click(object sender, RoutedEventArgs e)
+        {
+            string Eingabe;
+            int Klemmung;
+            string Schraubenlaenge;
+            Eingabe=txtBox_Laenge.Text;
+            if (CheckEingabe(Eingabe) == true)
+            {
+                Klemmung = Convert.ToInt16(txtBox_Laenge.Text);
+                Schraubenlaenge = "C:\\Kuhne Group\\Library\\DIN 933   SechskantschraubeISO 4017\\M16\\ISO 4017 - M16 x 25-C.sldprt";
+                SW_Schraube.OpenModel();
+                SW_Schraube.changeAndRebuild(Klemmung, Schraubenlaenge);
+            }
+            else return;
+
+            Environment.Exit(0);
+
+        }
+        private void btn_M16x30_Click(object sender, RoutedEventArgs e)
+        {
+            string Eingabe;
+            int Klemmung;
+            string Schraubenlaenge;
+            Eingabe = txtBox_Laenge.Text;
+            if (CheckEingabe(Eingabe) == true)
+            {
+                Klemmung = Convert.ToInt16(txtBox_Laenge.Text);
+                Schraubenlaenge = "C:\\Kuhne Group\\Library\\DIN 933   SechskantschraubeISO 4017\\M16\\ISO 4017 - M16 x 30-C.sldprt";
+                SW_Schraube.OpenModel();
+                SW_Schraube.changeAndRebuild(Klemmung, Schraubenlaenge);
+            }
+            else return;
+
+            Environment.Exit(0);
+
+        }
+        private void btn_M16x35_Click(object sender, RoutedEventArgs e)
+        {
+            string Eingabe;
+            int Klemmung;
+            string Schraubenlaenge;
+            Eingabe = txtBox_Laenge.Text;
+            CheckEingabe(Eingabe);
+            Klemmung = Convert.ToInt16(txtBox_Laenge.Text);
+            Schraubenlaenge = "C:\\Kuhne Group\\Library\\DIN 933   SechskantschraubeISO 4017\\M16\\ISO 4017 - M16 x 35-C.sldprt";
+            SW_Schraube.OpenModel();
+            SW_Schraube.changeAndRebuild(Klemmung, Schraubenlaenge);
+            Environment.Exit(0);
+        }
+        private void btn_M16x40_Click(object sender, RoutedEventArgs e)
+        {
+            string Eingabe;
+            int Klemmung;
+            string Schraubenlaenge;
+            Eingabe = txtBox_Laenge.Text;
+            if (CheckEingabe(Eingabe) == true)
+            {
+                Klemmung = Convert.ToInt16(txtBox_Laenge.Text);
+                Schraubenlaenge = "C:\\Kuhne Group\\Library\\DIN 933   SechskantschraubeISO 4017\\M16\\ISO 4017 - M16 x 40-C.sldprt";
+                SW_Schraube.OpenModel();
+                SW_Schraube.changeAndRebuild(Klemmung, Schraubenlaenge);
+            }
+            else return;
+
+            Environment.Exit(0);
+
+        }
+        private void btn_M16x45_Click(object sender, RoutedEventArgs e)
+        {
+            string Eingabe;
+            int Klemmung;
+            string Schraubenlaenge;
+            Eingabe = txtBox_Laenge.Text;
+            if (CheckEingabe(Eingabe) == true)
+            {
+                Klemmung = Convert.ToInt16(txtBox_Laenge.Text);
+                Schraubenlaenge = "C:\\Kuhne Group\\Library\\DIN 933   SechskantschraubeISO 4017\\M16\\ISO 4017 - M16 x 45-C.sldprt";
+                SW_Schraube.OpenModel();
+                SW_Schraube.changeAndRebuild(Klemmung, Schraubenlaenge);
+            }
+            else return;
+
+            Environment.Exit(0);
+
+        }
+        private void btn_M16x50_Click(object sender, RoutedEventArgs e)
+        {
+            string Eingabe;
+            int Klemmung;
+            string Schraubenlaenge;
+            Eingabe = txtBox_Laenge.Text;
+            if (CheckEingabe(Eingabe) == true)
+            {
+                Klemmung = Convert.ToInt16(txtBox_Laenge.Text);
+                Schraubenlaenge = "C:\\Kuhne Group\\Library\\DIN 933   SechskantschraubeISO 4017\\M16\\ISO 4017 - M16 x 50-C.sldprt";
+                SW_Schraube.OpenModel();
+                SW_Schraube.changeAndRebuild(Klemmung, Schraubenlaenge);
+            }
+            else return;
+
+            Environment.Exit(0);
+
+        }
+        private void btn_M16x55_Click(object sender, RoutedEventArgs e)
+        {
+            string Eingabe;
+            int Klemmung;
+            string Schraubenlaenge;
+            Eingabe = txtBox_Laenge.Text;
+            if (CheckEingabe(Eingabe) == true)
+            {
+                Klemmung = Convert.ToInt16(txtBox_Laenge.Text);
+                Schraubenlaenge = "C:\\Kuhne Group\\Library\\DIN 933   SechskantschraubeISO 4017\\M16\\ISO 4017 - M16 x 50-C.sldprt";
+                SW_Schraube.OpenModel();
+                SW_Schraube.changeAndRebuild(Klemmung, Schraubenlaenge);
+            }
+            else return;
+
+            Environment.Exit(0);
+
+        }
+        private void btn_M16x60_Click(object sender, RoutedEventArgs e)
+        {
+            string Eingabe;
+            int Klemmung;
+            string Schraubenlaenge;
+            Eingabe = txtBox_Laenge.Text;
+            if (CheckEingabe(Eingabe) == true)
+            {
+                Klemmung = Convert.ToInt16(txtBox_Laenge.Text);
+                Schraubenlaenge = "C:\\Kuhne Group\\Library\\DIN 933   SechskantschraubeISO 4017\\M16\\ISO 4017 - M16 x 60-C.sldprt";
+                SW_Schraube.OpenModel();
+                SW_Schraube.changeAndRebuild(Klemmung, Schraubenlaenge);
+            }
+            else return;
+
+            Environment.Exit(0);
+
+        }
+        private void btn_M16x65_Click(object sender, RoutedEventArgs e)
+        {
+            string Eingabe;
+            int Klemmung;
+            string Schraubenlaenge;
+            Eingabe = txtBox_Laenge.Text;
+            if (CheckEingabe(Eingabe) == true)
+            {
+                Klemmung = Convert.ToInt16(txtBox_Laenge.Text);
+                Schraubenlaenge = "C:\\Kuhne Group\\Library\\DIN 933   SechskantschraubeISO 4017\\M16\\ISO 4017 - M16 x 65-C.sldprt";
+                SW_Schraube.OpenModel();
+                SW_Schraube.changeAndRebuild(Klemmung, Schraubenlaenge);
+            }
+            else return;
+
+            Environment.Exit(0);
+
+        }
+        private void btn_M16x70_Click(object sender, RoutedEventArgs e)
+        {
+            string Eingabe;
+            int Klemmung;
+            string Schraubenlaenge;
+            Eingabe = txtBox_Laenge.Text;
+            if (CheckEingabe(Eingabe) == true)
+            {
+                Klemmung = Convert.ToInt16(txtBox_Laenge.Text);
+                Schraubenlaenge = "C:\\Kuhne Group\\Library\\DIN 933   SechskantschraubeISO 4017\\M16\\ISO 4017 - M16 x 70-C.sldprt";
+                SW_Schraube.OpenModel();
+                SW_Schraube.changeAndRebuild(Klemmung, Schraubenlaenge);
+            }
+            else return;
+
+            Environment.Exit(0);
+
+        }
+        private void btn_M16x80_Click(object sender, RoutedEventArgs e)
+        {
+            string Eingabe;
+            int Klemmung;
+            string Schraubenlaenge;
+            Eingabe = txtBox_Laenge.Text;
+            if (CheckEingabe(Eingabe) == true)
+            {
+                Klemmung = Convert.ToInt16(txtBox_Laenge.Text);
+                Schraubenlaenge = "C:\\Kuhne Group\\Library\\DIN 933   SechskantschraubeISO 4017\\M16\\ISO 4017 - M16 x 80-C.sldprt";
+                SW_Schraube.OpenModel();
+                SW_Schraube.changeAndRebuild(Klemmung, Schraubenlaenge);
+            }
+            else return;
+
+            Environment.Exit(0);
+
+        }
+        private void btn_M16x90_Click(object sender, RoutedEventArgs e)
+        {
+            string Eingabe;
+            int Klemmung;
+            string Schraubenlaenge;
+            Eingabe = txtBox_Laenge.Text;
+            if (CheckEingabe(Eingabe) == true)
+            {
+                Klemmung = Convert.ToInt16(txtBox_Laenge.Text);
+                Schraubenlaenge = "C:\\Kuhne Group\\Library\\DIN 933   SechskantschraubeISO 4017\\M16\\ISO 4017 - M16 x 90-C.sldprt";
+                SW_Schraube.OpenModel();
+                SW_Schraube.changeAndRebuild(Klemmung, Schraubenlaenge);
+            }
+            else return;
+
+            Environment.Exit(0);
+
+        }
+    }
+}
---
+++ Macro101_OpenM16/Macro101_OpenM16/SW_Schraube.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+
+namespace Macro101_OpenM16
+{
+    internal class SW_Schraube
+    {
+        
+        public static void OpenModel()
+        {
+        SldWorks swApp = new SldWorks();
+        ModelDoc2 swModel;
+        int errors = 0;
+        int warnings = 0;
+        string filepath = "C:\\Kuhne Group\\Sandkasten\\Krahn\\Macro´s SW\\OpenDIN934_M16\\DIN934_M16_Daten\\DIN934+Normteile_M16x25.SLDASM";
+        swModel = (ModelDoc2)swApp.ActivateDoc3("DIN934+Normteile_M16x25.SLDASM", false, 1, 0);
+
+        if (swModel == null)
+            {
+                swModel = (ModelDoc2)swApp.OpenDoc6(filepath, 2, 2, "", ref errors, ref warnings);
+            }
+        
+         swApp.ActivateDoc3("DIN934+Normteile_M16.SLDASM", false, 1, ref errors);
+        }
+        public static void CloseModel()
+        {
+            // schliesse das Model
+        }
+        public static void changeAndRebuild(int Klemmung, string SchraubenFile)
+        {
+            SldWorks swApp = new SldWorks();
+            ModelDoc2 swModel;
+            AssemblyDoc swAssy;
+            Dimension myDimension;
+            bool boolstatus;
+            swModel = (ModelDoc2)swApp.ActiveDoc;
+            //select Schraube
+            swModel.Extension.SelectByID2("", "COMPONENT", 0, 0, 0, false, 0, null, 0);
+            swAssy = (AssemblyDoc)swModel;
+            //ersetze Schraube
+            boolstatus = swAssy.ReplaceComponents2(SchraubenFile, "", true, 0, true);
+            //select Features für Abstand
+            swModel.Extension.SelectByID2("Abstand1", "MATE",0,0,0,false,0,null,0);
+            swModel.ActivateSelectedFeature();
+            swModel.Extension.SelectByID2("D1@Abstand1@DIN934+Normteile_M16x25.SLDASM","DIMENSION",0,0,0,false,0, null, 0);
+            myDimension = (Dimension)swModel.Parameter("D1@Abstand1");
+            //Ändere den Abstand
+            myDimension.SystemValue = Klemmung * 0.001;
+            swModel.ClearSelection2(true);
+            // Neuaufbau
+            swModel.ForceRebuild3(true);
+        }
+    }
+}
